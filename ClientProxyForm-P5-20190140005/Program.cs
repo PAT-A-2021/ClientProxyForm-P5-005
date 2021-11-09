6993b7ae52b7f58e117d4e8dfde8f011218405d0
ASUS <ASUS@LAPTOP-3882BD5J>
2021-11-09 19:34:15 +0700

    Add project files.

---
+++ ClientProxyForm-P5-20190140005/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using ServiceMtk_P1_20190140005;
+
+namespace ClientProxyForm_P5_20190140005
+{
+    static class Program
+    {
+       
+        [STAThread]
+        static void Main()
+        {
+            Application.EnableVisualStyles();
+            Application.SetCompatibleTextRenderingDefault(false);
+            Application.Run(new Form1());
+
+        }
+    }
+}
